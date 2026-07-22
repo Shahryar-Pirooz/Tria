eedcf006337f22bec2b9dd7cd1663f9d079209c2
Shahryar <dev.shahryar@gmail.com>
2026-07-22 15:19:57 +0330

    feat: add PWA support with VitePWA plugin and configure manifest

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,65 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   base: '/Tria/',
+
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        id: '/Tria/',
+
+        name: 'Tria',
+        short_name: 'Tria',
+        description:
+          'A deterministic, client-side password generator.',
+
+        theme_color: '#080808',
+        background_color: '#080808',
+
+        display: 'standalone',
+        start_url: '/Tria/',
+        scope: '/Tria/',
+
+        icons: [
+          {
+            src: 'logo.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(
+        new URL('./src', import.meta.url)
+      ),
     },
   },
 })
